4796932992241828aa355f30a3792f270547e46b
Romain PERRIN <romain.perrin@etu.unistra.fr>
2017-04-08 18:10:54 +0200

    Ajout de l'enregistrement auprès du coordinateur à l'initialisation du l'objet producteur

--- ObjetProducteur.java
+++ ObjetProducteur.java
@@ -1,23 +1,30 @@
 import java.net.* ;
 import java.rmi.* ;
+import java.net.MalformedURLException ; 
 
 public class ObjetProducteur
 {
 	public static void main(String [] args)
 	{
-		if (args.length != 4)
+		if (args.length != 5)
 		{
-			System.out.println("Usage : java ObjectProducteur <port rmiregistry> <idProducteur> <typeRessource> <quantiteRessource>") ;
+			System.out.println("Usage : java ObjectProducteur <machine du Serveur> <port rmiregistry> <idProducteur> <typeRessource> <quantiteRessource>") ;
 			System.exit(0) ;
 		}
 		try
 		{
-			int idProducteur = Integer.parseInt(args[1]);
-			int quantiteRessource = Integer.parseInt(args[3]);
-			ProducteurImpl objLocal = new ProducteurImpl(idProducteur, args[2], quantiteRessource );
+			Coordinateur coordinateur = (Coordinateur) Naming.lookup( "rmi://" + args[0] + ":" + args[1] + "/objLocal" );
+			
+			int idProducteur = Integer.parseInt(args[2]);
+			int quantiteRessource = Integer.parseInt(args[4]);
+			ProducteurImpl objLocal = new ProducteurImpl(idProducteur, args[3], quantiteRessource );
 			Naming.rebind( "rmi://localhost:" + args[0] + "/objLocal" ,objLocal) ;
 			System.out.println("Producteur " + objLocal.getIdProducteur() + " pret") ;
+			
+			// s'enregistrer auprès du coordinateur (convention : port 9000)
+			coordinateur.identifierProducteur(objLocal.getIdProducteur(), objLocal.getNumeroPort());
 		}
+		catch (NotBoundException re) { System.out.println(re) ; }
 		catch (RemoteException re) { System.out.println(re) ; }
 		catch (MalformedURLException e) { System.out.println(e) ; }
 	}
