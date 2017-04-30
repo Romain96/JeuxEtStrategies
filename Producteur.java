8f50d5adaedef956ba39a9a6b2be489f32a56556
Romain PERRIN <romain.perrin@etu.unistra.fr>
2017-04-30 22:19:57 +0200

    Ajout d'une méthode observer dans l'interface Producteur

--- Producteur.java
+++ Producteur.java
@@ -8,4 +8,7 @@ public interface Producteur extends Remote
 	
 	// appelé par les agents pour acquérir des ressources
 	public int attribuerRessources(String typeRessource, int quantiteDemandee) throws RemoteException;
+	
+	// appelé par les agents pour observer le producteur (type et quantité de ressource disponible)
+	public Ressource observer(int idAgent) throws RemoteException;
 }
