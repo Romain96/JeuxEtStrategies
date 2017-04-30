d12e5253116fbdd0107c3f030c0073810b3d7afc
Romain PERRIN <romain.perrin@etu.unistra.fr>
2017-04-30 15:58:26 +0200

    Ressource et non ressources dans setRessourcesByTYpe

--- AgentImpl.java
+++ AgentImpl.java
@@ -146,7 +146,7 @@ public abstract class AgentImpl
 		int pos = -1;
 		for (int i = 0; i < this.ressources.size() && pos < 0; i++)
 		{
-			if (this.ressources.get(i).getTypeRessource().equals(ressources.getTypeRessource()))
+			if (this.ressources.get(i).getTypeRessource().equals(ressource.getTypeRessource()))
 			{
 				pos = i;
 			}
