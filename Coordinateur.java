2c3288a28c66651e84b00e1be3f11a59e9db844e
Romain PERRIN <romain.perrin@etu.unistra.fr>
2017-05-01 17:55:13 +0200

    encore des IOException

--- Coordinateur.java
+++ Coordinateur.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 public interface Coordinateur extends Remote
 {
 	// appelé par les agents pour s'enregistrer auprès du coordinateur
-	public void identifierAgent(int idAgent) throws RemoteException;
+	public void identifierAgent(int idAgent) throws RemoteException, IOException;
 	
 	// appelé par les producteurs pour s'enregistrer auprès du coordinateur
-	public void identifierProducteur(int idProducteur) throws RemoteException;
+	public void identifierProducteur(int idProducteur) throws RemoteException, IOException;
 	
 	// appelé par les agents pour signaler la fin de leur tour
 	public void signalerFinTour(int idAgent, String log) throws RemoteException, IOException;
