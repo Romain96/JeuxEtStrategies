5e28c6fa69db2b055f717abe2cc9e47fdcc842b2
Romain PERRIN <romain.perrin@etu.unistra.fr>
2017-05-01 18:29:50 +0200

    correction de l'erreur this

--- ProducteurImpl.java
+++ ProducteurImpl.java
@@ -110,7 +110,7 @@ public class ProducteurImpl extends UnicastRemoteObject implements Producteur
 		// l'agent ne reçoit que le maximum disponible
 		if (this.quantiteRessource <= quantiteDemandee)
 		{
-			quantiteAttribuee = Math.min(this.quantiteDemandee, this.quantiteMax);
+			quantiteAttribuee = Math.min(quantiteDemandee, this.quantiteMax);
 			this.quantiteRessource = 0;
 			return quantiteAttribuee;
 		}
